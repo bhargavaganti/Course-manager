decec30432eebe470038d174b431e2c68dfba133
kizto86 <kingchuks@hotmail.com>
2020-11-11 15:30:25 +0100

    :construction:course detail page on progress

--- src/components/CourseDetail.js
+++ src/components/CourseDetail.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const CourseDetail = ({ match }) => {
-  const [courseDetail, setCourseDetail] = useState({});
+  const [course, setCourse] = useState({});
 
   useEffect(() => {
     getCourseDetails();
+    console.log(match);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -13,7 +14,8 @@ const CourseDetail = ({ match }) => {
     axios
       .get(`http://localhost:5000/api/courses/${match.params.id}`)
       .then((response) => {
-        setCourseDetail(response.data);
+        console.log(response.data);
+        setCourse(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -21,9 +23,32 @@ const CourseDetail = ({ match }) => {
   };
   return (
     <div>
-      <h1>Course Detail Page</h1>
-      <h2>{courseDetail.title}</h2>
-      <h2>{courseDetail.estimatedTime}</h2>
+      <div className="actions-bar">
+        <div className="bounds">
+          <div className="grid-100">
+            <span>
+              <a className="button" href="update-course.html">
+                Update Course
+              </a>
+              <a className="button" href="#">
+                Delete Course
+              </a>
+            </span>
+            <a className="button button-secondary" href="/">
+              Return to List
+            </a>
+          </div>
+          <div className="bounds course--detail">
+            <div className="grid-66">
+              <div className="course--header">
+                <h4 className="course--label">Course</h4>
+                <h3 class="course--title">{course.title}</h3>
+                <p>By Paula Chuks</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
